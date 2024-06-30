718b7c8c04288ff16329b0307bae3b61dca626b7
yin-lo <yinlo@hotmail.fr>
2024-06-30 21:24:27 +0200

    24-variables-scss / base of the style files ready

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,5 @@
-import logo from '../../assets/logo.svg';
+import { Container } from 'react-bootstrap';
+import logo from '../../assets/logo/svg/logo1_bleuvert.svg';
 
 import './App.scss';
 
@@ -7,10 +8,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Container fluid="md">
+          <h1>h1 coucou</h1>
+          <h2>h2 coucou</h2>
+          <h3>h3 coucou</h3>
+          <h4>h4 coucou</h4>
+          <p className="text-chapo">test .text-chapo</p>
+          <p className="text-corpus">test .text-corpus</p>
+          <p className="text-legend">test .text-legend</p>
 
-        <p>
-          Edit <code>src/components/App/App.tsx</code> and save to reload.
-        </p>
+          <p> Edit and save to reload.</p>
+        </Container>
 
         <a
           className="App-link"
