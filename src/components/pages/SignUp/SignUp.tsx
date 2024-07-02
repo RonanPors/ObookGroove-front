83d3038a09dd3cf50b54eed94d6e3cee4b47dec0
louise1991 <lelivrelouise@gmail.com>
2024-07-02 17:00:06 +0200

    signin form react component initialized

--- src/components/pages/SignUp/SignUp.tsx
+++ src/components/pages/SignUp/SignUp.tsx
@@ -1,7 +1,36 @@
+import { FormField, Button, Checkbox, Form, Segment, Header } from 'semantic-ui-react'
+
 export default function SignUp (){
   return (
-    <>
-    
-  </>
-  );
+  <Segment inverted>
+     <Header as='h1'>S'inscrire</Header>
+    <Form inverted size='large'>
+    <FormField >
+      <label>email@domain.com</label>
+      <input placeholder='email@domain.com' />
+    </FormField>
+    <FormField>
+      <label>pseudo</label>
+      <input placeholder='pseudo' />
+    </FormField>
+    <FormField>
+      <label>Numéro de téléphone mobile</label>
+      <input placeholder='Numéro de mobile' />
+    </FormField>
+    <FormField>
+      <label>Password</label>
+      <input placeholder='Password' />
+    </FormField>
+    <FormField>
+      <label>Confirm password</label>
+      <input placeholder='Confirm password' />
+    </FormField>
+    <FormField>
+      <Checkbox label='I agree to the Terms and Conditions' />
+    </FormField>
+    <Button type='submit'>Créer un compte</Button>
+   </Form>
+  </Segment>
+  
+)
 }
