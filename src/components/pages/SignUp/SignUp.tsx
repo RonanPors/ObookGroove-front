d40fe5a703f9585cb5dfbf3c30b4d12055c2aeff
yin-lo <yinlo@hotmail.fr>
2024-07-03 11:34:06 +0200

    7-inscription_ form with field in French

--- src/components/pages/SignUp/SignUp.tsx
+++ src/components/pages/SignUp/SignUp.tsx
@@ -1,37 +1,51 @@
-import { FormField, Button, Checkbox, Form, Segment, Header } from 'semantic-ui-react'
+import {
+  FormField,
+  Button,
+  Checkbox,
+  Form,
+  Segment,
+  Header,
+} from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
 import './SignUp.scss';
 
-export default function SignUp (){
+export default function SignUp() {
   return (
-  <Segment inverted className='Segment__SignUp'>
-        <Form inverted size='large' className='Form__SignUp'>
-        <Header inverted as='h1' className='h1__header'>Créer un compte</Header>
-            <FormField >
-              <label>email@domain.com</label>
-              <input placeholder='email@domain.com' />
-            </FormField>
-            <FormField>
-              <label>pseudo</label>
-              <input placeholder='pseudo' />
-            </FormField>
-            <FormField>
-              <label>Numéro de téléphone mobile</label>
-              <input placeholder='Numéro de mobile' />
-            </FormField>
-            <FormField>
-              <label>Password</label>
-              <input placeholder='Password' />
-            </FormField>
-            <FormField>
-              <label>Confirm password</label>
-              <input placeholder='Confirm password' />
-            </FormField>
-            <FormField>
-              <Checkbox label='I agree to the Terms and Conditions' />
-            </FormField>
-        <Button type='submit'>Créer un compte</Button>
+    <Segment inverted className="Segment__SignUp">
+      <Form inverted size="large" className="Form__SignUp">
+        <Header inverted as="h1" className="h1__header">
+          Créer un compte
+        </Header>
+        <FormField>
+          <label htmlFor="email">Email</label>
+          <input placeholder="email@domain.com" id="email" />
+        </FormField>
+        <FormField>
+          <label htmlFor="pseudo">Pseudo</label>
+          <input placeholder="Pseudo" id="pseudo" />
+        </FormField>
+        <FormField>
+          <label htmlFor="phone-mobile">Numéro de téléphone mobile</label>
+          <input placeholder="06 00 00 00 00" id="phone-mobile" />
+        </FormField>
+        <FormField>
+          <label htmlFor="password">Mot de passe</label>
+          <input placeholder=" xxxxxxx" id="password" />
+        </FormField>
+        <FormField>
+          <label htmlFor="confirm-password">Confirmer votre mot de passe</label>
+          <input placeholder="xxxxxxxx" id="confirm-password" />
+        </FormField>
+        <FormField>
+          <Checkbox
+            as={Link}
+            to="/general-conditions-use"
+            label="J'accepte les conditions d'utilisation"
+          />
+        </FormField>
+        <Button type="submit">Créer un compte</Button>
       </Form>
-  </Segment>
-  
-)
+    </Segment>
+  );
 }
