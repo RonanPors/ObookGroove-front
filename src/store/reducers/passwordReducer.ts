c601d6fd7b46c769287979032052182a60e1fc2f
yin-lo <yinlo@hotmail.fr>
2024-07-05 00:52:40 +0200

    41-create NewPassword page

--- src/store/reducers/passwordReducer.ts
+++ src/store/reducers/passwordReducer.ts
@@ -1,24 +1,36 @@
 import { createAction, createReducer } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { createAppAsyncThunk } from '../../hooks/redux';
+import { Credentials } from '../../@types/user';
 
 type PasswordReducerState = {
   loading: boolean;
   error: string;
-  email: string;
+  credentials: Credentials;
+  confirmPassword: string;
 };
 
 const initialState: PasswordReducerState = {
   loading: false,
   error: '',
-  email: '',
+  credentials: {
+    email: '',
+    password: '',
+  },
+  confirmPassword: '',
 };
+
+export type KeysOfCredential = keyof Credentials;
 
 // actions pour modifier les champs du formulaire
 export const updateField = createAction<{
-  field: string;
+  field: KeysOfCredential;
   value: string;
 }>('RESET_PASSWORD/UPDATE_FIELD');
+
+export const updateConfirmPassword = createAction<{
+  value: string;
+}>('RESET_PASSWORD/UPDATE_CONFIRM_PASSWORD');
 
 /*
 ------------------- RESET ----------------------
@@ -30,7 +42,7 @@ export const resetPassword = createAppAsyncThunk(
   async (_, thunkAPI) => {
     const store = thunkAPI.getState();
     const body = {
-      email: store.password.email,
+      email: store.password.credentials.email,
     };
     const { data } = await axios.post(
       `${import.meta.env.VITE_API_URL}/auth/reset-password`,
@@ -40,9 +52,45 @@ export const resetPassword = createAppAsyncThunk(
     return data;
   }
 );
+/*
+------------------- NEW PASSWORD ----------------------
+*/
+
+type NewArgs = {
+  userId: string;
+  resetToken: string;
+};
+
+export const newPassword = createAppAsyncThunk(
+  'NEW_PASSWORD/NEW_PASSWORD_ASYNC',
+  async ({ userId, resetToken }: NewArgs, thunkAPI) => {
+    console.log(userId, resetToken);
+    const store = thunkAPI.getState();
+    const body = {
+      password: store.password.credentials.password,
+      confirmPassword: store.password.confirmPassword,
+    };
+
+    const { data } = await axios.post(
+      `${import.meta.env.VITE_API_URL}/auth/reset-password/${userId}/${resetToken}`,
+      body
+    );
+
+    console.log(data);
+    return data;
+  }
+);
 
 const passwordReducer = createReducer(initialState, (builder) => {
   builder
+    .addCase(updateField, (state, action) => {
+      // le payload correspond aux données de l'action asynchrone
+      const { field } = action.payload;
+      state.credentials[field] = action.payload.value;
+    })
+    .addCase(updateConfirmPassword, (state, action) => {
+      state.confirmPassword = action.payload.value;
+    })
     /* -----------------------------
     ------------- RESET --------------
     --------------------------------- */
