dcaa63da0f29d26b7bc7d624b292eb94a0f41035
yin-lo <yinlo@hotmail.fr>
2024-07-08 19:57:04 +0200

    install graph and user request example

--- src/components/pages/Landing/Landing.tsx
+++ src/components/pages/Landing/Landing.tsx
@@ -1,9 +1,15 @@
+import { useUserByIdQuery } from '../../../hooks/graphql';
 
-export default function Landing (){
+export default function Landing() {
+  const { user, loading, error } = useUserByIdQuery(1);
+
   return (
     <>
-    
-  </>
+      {error && <p> Une erreur utilisateur</p>}
+      <div>
+        {loading && !error && <p>Chargement...</p>}
+        {!loading && !error && <p>Bienvenue {user?.pseudo}</p>}
+      </div>
+    </>
   );
 }
-
---
+++ src/lib/gql/queries.ts
@@ -0,0 +1,52 @@
+import {
+  ApolloClient,
+  createHttpLink,
+  gql,
+  concat,
+  ApolloLink,
+  InMemoryCache,
+} from '@apollo/client';
+
+// création de la base url pour les requêtes graphql
+const httpLink = createHttpLink({
+  uri: `${import.meta.env.VITE_API_URL}/graphql`,
+});
+
+// instance de classe qui va effectuer une action avant la requête
+const authLink = new ApolloLink((operation, forward) => {
+  console.log('[customLink] operation', operation);
+
+  return forward(operation);
+});
+
+// le client qui va effectuer ces actions avant chaque requête
+export const apolloClient = new ApolloClient({
+  link: concat(authLink, httpLink),
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: 'no-cache',
+    },
+    query: {
+      fetchPolicy: 'no-cache',
+    },
+  },
+});
+
+// fragment pour une partie de la requête
+const userDetailsFragment = gql`
+  fragment UserDetails on User {
+    id
+    pseudo
+  }
+`;
+// la requête
+export const userByIdQuery = gql`
+  query UserById($id: Int!) {
+    user(id: $id) {
+      ...UserDetails
+    }
+  }
+  # la requête attend des arguments :
+  ${userDetailsFragment}
+`;
