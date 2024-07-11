fb0762f8627f768844ce1bc962ddfbd16fd0518b
yin-lo <yinlo@hotmail.fr>
2024-07-11 12:24:17 +0200

    53-try to display dynamic data on cardBook

--- src/components/elements/Card/Card.tsx
+++ src/components/elements/Card/Card.tsx
@@ -1,66 +1,52 @@
-import React from 'react'
-import './Card.scss'
+import React from 'react';
+import './Card.scss';
 import {
-  CardMeta,
-  CardHeader,
   Header,
-  CardDescription,
   CardContent,
   Card,
-  Icon,
   Image,
   Segment,
-  Label, 
-  LabelGroup, 
-  Item, 
-  ItemImage
-} from 'semantic-ui-react'
+  Label,
+  LabelGroup,
+  Item,
+  ItemImage,
+} from 'semantic-ui-react';
 
 import Dune from '../../../assets/illustrations/jacket-livre/Dune.jpg';
-import BookShell from '../../../assets/logo/svg/bookshell 1.svg'
+import BookShell from '../../../assets/logo/svg/bookshell 1.svg';
+// essai graphql:
+import { useUserByIdQuery } from '../../../hooks/graphql';
 
 export default function CardBook() {
-    return (
-   
-    <Card id='card__container' fluid>
-        <Segment id='card__image' inverted>
-            <Item align='centered'>
-              <ItemImage src={Dune} wrapped ui={false} size='small' centered />
-            </Item>
-        </Segment>
-        <Icon >
-            
-        </Icon>
-        {/* <Segment inverted> */}
-          <CardContent>
-             <a>
-               <Image floated='right' size='mini' src={BookShell} />
-             </a>
-             <Header inverted color='grey' id='card_author' as='h1'>Dune</Header>
-             <Header inverted color='grey' as='h3'>Franck Herbert</Header>  
-         </CardContent>
-        {/* </Segment> */}
-    
-        <CardContent extra>
-            <LabelGroup color='blue'>
-              <Label>
-                Science Fiction
-              </Label>
-              <Label>
-                Aventure
-              </Label>
-              <Label>
-                Drame
-              </Label>
-              <Label>
-                Fantasy
-              </Label>
-            </LabelGroup>
-        </CardContent>
+  const { user } = useUserByIdQuery(2);
+  return (
+    <Card id="card__container" fluid>
+      <Segment id="card__image" inverted>
+        <Item align="centered">
+          <ItemImage src={Dune} wrapped ui={false} size="small" centered />
+        </Item>
+      </Segment>
+
+      <CardContent>
+        <div>
+          <Image floated="right" size="mini" src={BookShell} />
+        </div>
+        <Header inverted color="grey" id="card_author" as="h1">
+          {user?.book.title}
+        </Header>
+        <Header inverted color="grey" as="h3">
+          Franck Herbert
+        </Header>
+      </CardContent>
+
+      <CardContent extra>
+        <LabelGroup color="blue">
+          <Label>Science Fiction</Label>
+          <Label>Aventure</Label>
+          <Label>Drame</Label>
+          <Label>Fantasy</Label>
+        </LabelGroup>
+      </CardContent>
     </Card>
-
- )
+  );
 }
-  
-
-
